48d94fa485f8725c3d10f5b9a588858422f53b44
Rav Smy <RavSmy@users.noreply.github.com>
2019-03-04 02:47:34 -0800

    Create 561_hw3_smyth.py

---
+++ 561_hw3_smyth.py
@@ -0,0 +1,26 @@
+#  561_hw3_smyth.py
+#  Smyth, Ravela
+#  CS561 HW3 "Standard Deviation"
+
+import math
+
+f = open('input')
+data = []
+mean = 0
+for line in f:
+	for num in line.split(' '):
+ 		data.append(int(num))
+		mean += int(num)
+		print(num)
+f.close()
+
+
+mean /= len(data)
+stddev = 0
+
+for num in data:
+	stddev += (num - mean) * (num - mean)
+stddev = math.sqrt(stddev / len(data))
+
+print("Your standard deviation is " , stddev)
+
